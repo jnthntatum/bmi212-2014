74de575df718d57586de65873efbcb573987d6b3
Jon Tatum <jdtatum@stanford.edu>
2014-04-14 11:32:41 -0700

    check and load appropriate functions for python 2 v 3

--- utils.py
+++ utils.py
@@ -4,17 +4,23 @@
 # helper functions that are useful in different modules (and probably ugly)
 
 from collections import namedtuple
-import urllib
 import config
+import sys
+import urllib
 
-SCHIZO_DRUGS = './schizo_drugs.dat'
-DEFAULT_CRED = './jtatum.cred'
+version = sys.version_info
 
-def url_encode( url ):
-	return urllib.parse.quote( url )
+if ( version[0] >= 3 and version[1] >= 2 ):
+	def url_encode( url ):
+		return urllib.parse.quote( url )
+elif ( version[0] >= 2 and version[1] >= 6 ):
+	def url_encode( url ):
+		return urllib.quote(url)
+else:
+	print ("Error: unsupported version. use python > 2.6")
 
 
-def load_drug_file( fname = SCHIZO_DRUGS ):
+def load_drug_file( fname = config.SCHIZO_DRUGS ):
 	result = {}
 	with open(fname, 'r') as file:
 		for line in file:
@@ -27,7 +33,7 @@ def load_drug_file( fname = SCHIZO_DRUGS ):
 
 OAuthCredentials = namedtuple('credentials', ('client_key', 'client_secret', 'access_token', 'access_token_secret'))
 
-def load_credentials_file ( fname = DEFAULT_CRED ):
+def load_credentials_file ( fname = config.DEFAULT_CRED ):
 	result = None	
 	with open(fname, 'r') as f:
 		row = f.readline().split(',')
