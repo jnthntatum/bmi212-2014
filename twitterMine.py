a0620bfc216fc6d274545941120d8deadaf55c97
Jon Tatum <jdtatum@stanford.edu>
2014-04-14 10:30:06 -0700

    more example code

--- twitterMine.py
+++ twitterMine.py
@@ -16,11 +16,30 @@ def main():
 	print(query)
 
 	# additional parameters can control date range, guessed language, etc. 
-	results = api_client.search( q = query, count = 60)
+	results = api_client.search( q = query, count = 100, lang="en")
 
 	for result in results['statuses']:
 		print (result['text']) 
+		print ("")
+
+	tl = get_timeline(api_client, '1265683232')
+	tlt = [t['text'] for t in tl]
+	print (tlt)
+
 	
+def setup(file = utils.DEFAULT_CRED):
+	creds = utils.load_credentials_file()
+	api_client = twython.Twython(*creds)
+	return api_client
+
+def get_followers_and_following(client, uid):
+	a = client.get_friends_ids(user_id = uid)['ids']
+	b = client.get_followers_ids(user_id = uid)['ids']
+	return set(a) & set(b)
+
+def get_timeline(client, uid):
+	blob = client.get_user_timeline(user_id = uid)
+	return blob
 
 if __name__ == "__main__":
 	main() 
